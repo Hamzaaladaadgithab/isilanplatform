5e4861c94c3d8e1f2909947345e26613018d079b
Hamzaaladaadgithab <aladaada5105390@gmail.com>
2025-10-09 15:10:39 +0300

    Yönetici paneli arayüzü ve CRUD işlevselliği uygulandı
    Company, JobCategory, JobVacancy, JobApplication ve User modelleri için resource controller’lar oluşturuldu.
    
    Tüm CRUD işlemleri için web.php dosyasında resource route’lar ayarlandı.
    
    Her kaynak için Blade görünümleri (index, create, edit) geliştirildi.
    
    Ana navigasyon düzeni, tüm yeni yönetim bölümlerine bağlantılar içerecek şekilde güncellendi.
    
    Arka ofise erişimi kısıtlamak için genel kullanıcı kaydı kaldırıldı.
    
    Ana giriş sayfası olarak özel bir kontrol paneli (dashboard) uygulandı.

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,17 +1,25 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+        Route::get('/', action: [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/companies', action: [CompanyController::class, 'index'])->name('company.index');
+        Route::get('/job-applications', action: [JobApplicationController::class, 'index'])->name('job-application.index');
+        Route::get('/job-categories', action: [JobCategoryController::class, 'index'])->name('job-category.index');
+        Route::get('/job-vacancies', action: [JobVacancyController::class, 'index'])->name('job-vacancy.index');
+        Route::get('/users', action: [UserController::class, 'index'])->name('user.index');
+
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
